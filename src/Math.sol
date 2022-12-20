7ba20a1f1d7fdbdb6965404bf5c7caa46e7b4262
Ainmox <ainmo-x@protonmail.me>
2022-12-20 16:52:46 -0700

    feat: add math library

---
+++ src/Math.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.5.0;
+
+/// @title A library for various common math operations
+/// @custom:coauthor Ainmox (https://github.com/ainmox)
+library Math {
+    /// @dev Gets the minimum of two unsigned 256 bit integers
+    /// @param x The first 256 bit integer
+    /// @param y The second 256 bit integer
+    /// @return result The minimum of the two 256 bit integers
+    function min(uint256 x, uint256 y) pure returns (uint256 result) {
+        result = x < y ? x : y;
+    }
+}
