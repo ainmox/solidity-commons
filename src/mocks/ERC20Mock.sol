a62aadb4b6ba0c6cbb4c806296c63025941ea905
Ainmox <ainmo-x@proton.me>
2023-03-09 03:26:48 -0700

    feat: make `ERC20Mock` implement `IERC20Optional`

--- src/mocks/ERC20Mock.sol
+++ src/mocks/ERC20Mock.sol
@@ -1,8 +1,30 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import {ERC20} from "../ERC20.sol";
+import {IERC20Optional} from "solidity-standard-interfaces/IERC20Optional.sol";
 
-contract ERC20Mock is ERC20 {
+contract ERC20Mock is ERC20, IERC20Optional {
+    /// @inheritdoc IERC20Optional
+    string public name;
+
+    /// @inheritdoc IERC20Optional
+    string public symbol;
+
+    /// @inheritdoc IERC20Optional
+    uint8 public immutable decimals;
+
+    struct Config {
+        string name;
+        string symbol;
+        uint8 decimals;
+    }
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) {
+        name     = name_;
+        symbol   = symbol_;
+        decimals = decimals_;
+    }
+
     function mint(address recipient, uint256 amount) public {
         _mint(recipient, amount);
     }
